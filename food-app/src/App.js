62312f6805fb5e01c4c0fa9eaf02fec79478206e
oluwapelumi1 <oluwapelumidaniel88@gmail.com>
2025-02-16 19:29:11 +0100

    commit

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,10 +1,16 @@
+import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
-
-
+import Cards from "./Components/Cards";
+import Food from "./Components/Food";
+import Category from "./Components/Category";
 function App() {
   return (
     <div className="App">
      <Navbar/> 
+     <Hero/>
+     <Cards/>
+     <Food/>
+     <Category/>
     </div>
   );
 }
